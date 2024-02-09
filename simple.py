f71a5c5439524451fb524a5012ccdb7b620013ab
Alexander Khouw <alexanderkhouw@alexanders-air.home>
2024-02-09 16:15:54 +0100

    First Commit

---
+++ simple.py
@@ -0,0 +1,10 @@
+#comment
+print("Hello world!")
+
+def function1():
+    print('In function 1')
+def function2(a):
+    print('Ik print nu de tekst:'+ a)
+function1()
+function2('oop')
+getal1 = 12
